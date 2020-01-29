4cf689af0e4503d4a463df18a26f30048c80e1c2
Julie Mugira <julie.mugira@andela.com>
2020-01-29 15:35:49 +0300

    Make Payment class into abstract base class, create class PaymentByCard to inherit from Payment

--- src/bricklane_platform/models/payment.py
+++ src/bricklane_platform/models/payment.py
@@ -1,24 +1,26 @@
 from decimal import Decimal
 from dateutil.parser import parse
-
-
+from abc import abstractmethod, ABCMeta
 from bricklane_platform.models.card import Card
 from bricklane_platform.config import PAYMENT_FEE_RATE
 
 
 class Payment(object):
 
+    __metaclass__ = ABCMeta
+
     customer_id = None
     date = None
     amount = None
     fee = None
-    card_id = None
+    # card_id = None
 
-    def __init__(self, data=None):
+    # def __init__(self, data=None):
 
-        if not data:
-            return
-
+    #     if not data:
+    #         return
+    @abstractmethod
+    def __init__(self, data)
         self.customer_id = int(data["customer_id"])
         self.date = parse(data["date"])
 
@@ -26,6 +28,19 @@ class Payment(object):
         self.fee = total_amount * PAYMENT_FEE_RATE
         self.amount = total_amount - self.fee
 
+    @abstractmethod
+    def is_successful(self):
+        pass
+
+
+Class PaymentByCard(payment)
+    name = "card"
+    card_id = None
+
+    def __init__(self, data=None);
+        if not data:
+            return
+        super(PaymentByCard, self).__init__(data)
         card = Card()
         card.card_id = int(data["card_id"])
         card.status = data["card_status"]
